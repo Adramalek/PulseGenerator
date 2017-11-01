8fb1bd7ac622382726470ff0c3cd9f53adb5669f
veresen <lolmariya@gmail.com>
2017-11-01 17:23:50 +0300

    Complete multi pulse generator

--- src/User/main.c
+++ src/User/main.c
@@ -20,20 +20,31 @@
  
 #include "main.h"
 
-TIM_Data training_timer = {41999, TRAINING_TIME*1000-1, TIM5, RCC_APB1Periph_TIM5, TIM5_IRQn};
-TIM_Data delay_timer = {83, DELAY*1000-1, TIM3, RCC_APB1Periph_TIM3, TIM3_IRQn};
-TIM_Data pre_timer = {83, 19999, TIM4, RCC_APB1Periph_TIM4, TIM4_IRQn};
-TIM_Data post_timer = {83, 19999, TIM2, RCC_APB1Periph_TIM2, TIM2_IRQn};
+uint8_t DELAY = 12; // in milliseconds
+uint8_t PULSE_DELAY = 40; // in milliseconds
+uint8_t TRAINING_TIME = 30; // in seconds (32 sec is max value)
+
+//PULSE_DELAY*1000-1 (PULSE_DELAY+DELAY)*1000-1
+//static TIM_Data training_timer = {41999, TRAINING_TIME*1000-1, TIM_CounterMode_Down, TIM5, RCC_APB1Periph_TIM5, TIM5_IRQn};
+static TIM_Data pulse_delay = {83, 0, TIM_CounterMode_Down, TIM5, RCC_APB1Periph_TIM5, TIM5_IRQn};
+static TIM_Data delay_timer = {83, 0, TIM_CounterMode_Up, TIM3, RCC_APB1Periph_TIM3, TIM3_IRQn};
+static TIM_Data pre_timer = {83, 19999, TIM_CounterMode_Up, TIM4, RCC_APB1Periph_TIM4, TIM4_IRQn};
+static TIM_Data post_timer = {83, 19999, TIM_CounterMode_Up, TIM2, RCC_APB1Periph_TIM2, TIM2_IRQn};
+
+void Init(){
+	pulse_delay.Period = (PULSE_DELAY+DELAY)*1000-1;
+	delay_timer.Period = DELAY*1000-1;
+}
 
 void Init_Timer(TIM_Data *timer){
 	RCC_APB1PeriphClockCmd(timer->RCC_APB1Periph_TIM, ENABLE);
 	TIM_TimeBaseInitTypeDef timerInitStructure; 
 	timerInitStructure.TIM_Prescaler = timer->Prescaler;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          ;
-	timerInitStructure.TIM_CounterMode = TIM_CounterMode_Up;
+	timerInitStructure.TIM_CounterMode = timer->TIM_CounterMode;
 	timerInitStructure.TIM_Period = timer->Period;
 	timerInitStructure.TIM_ClockDivision = 0;
 	timerInitStructure.TIM_RepetitionCounter = 0;
-	TIM_TimeBaseInit(TIM5, &timerInitStructure);
+	TIM_TimeBaseInit(timer->TIM, &timerInitStructure);
 	
 	NVIC_InitTypeDef nvicStructure;
 	nvicStructure.NVIC_IRQChannel = timer->NVIC_TIM_IRQChannel;
@@ -47,15 +58,15 @@ void Init_Timer(TIM_Data *timer){
   TIM_ITConfig (timer->TIM, TIM_IT_Update, ENABLE);
 }
 
-void InitTrainingTimer(){
-	Init_Timer(&training_timer);
+void Init_pulse_delay(){
+	Init_Timer(&pulse_delay);
 }
 
-void InitializeDelay(){
+void Initialize_Delay(){
 	Init_Timer(&delay_timer);
 }
 
-void InitializePost()
+void Initialize_Post()
 {
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 	GPIO_InitTypeDef gpioStructure;
@@ -67,10 +78,8 @@ void InitializePost()
 	Init_Timer(&post_timer);
 }
 
-void InitializePre()
+void Initialize_Pre()
 {
-	GPIO_InitTypeDef gpio_C; 
-	
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 	GPIO_InitTypeDef gpioStructure;
 	gpioStructure.GPIO_Pin = GPIO_Pin_12;
@@ -80,7 +89,6 @@ void InitializePre()
 
 	Init_Timer(&pre_timer);
 }
-
 
 void TIM2_IRQHandler(){
 	if (TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET){
@@ -110,14 +118,44 @@ void TIM4_IRQHandler(){
 	}
 }
 
+void TIM5_IRQHandler(){
+		if (TIM_GetITStatus(TIM5, TIM_IT_Update) != RESET){
+			TIM_ClearITPendingBit(TIM5, TIM_IT_Update);
+			Start_Timers();
+		}
+}
+
+inline void Stop_Timers(){
+	TIM_Cmd(TIM5, DISABLE);
+	TIM4_IRQHandler();
+	TIM_Cmd(TIM3, DISABLE);
+	TIM2_IRQHandler();
+}
+
+inline void Start_Timers(){
+	TIM_Cmd(TIM4, ENABLE);
+	GPIO_WriteBit(GPIOD, GPIO_Pin_12, Bit_SET);
+	TM_DISCO_LedOn(LED_RED);
+	TIM_Cmd(TIM3, ENABLE);
+}
+
 void TM_EXTI_Handler(uint16_t GPIO_Pin){
 	if (GPIO_Pin == GPIO_Pin_0){
-		TIM_Cmd(TIM4, ENABLE);
-		GPIO_WriteBit(GPIOD, GPIO_Pin_12, Bit_SET);
-		TM_DISCO_LedOn(LED_RED);
-		TIM_Cmd(TIM3, ENABLE);
+		if (Mode == MULTI){
+			static uint8_t stop_timer = 0;
+			if (stop_timer){
+				Stop_Timers();
+			} else {
+				TIM_Cmd(TIM5, ENABLE);
+			}
+			stop_timer = !stop_timer;
+		} else {
+			Start_Timers();
+		}
 	}
 }
+
+
 
 int main(void) {
 	//PD12 -- Pre, PD15 - Post
@@ -128,9 +166,11 @@ int main(void) {
 	TM_DISCO_LedInit();
 	TM_DISCO_ButtonInit();
 	
-	InitializeDelay();
-	InitializePre();
-	InitializePost();
+	Init();
+	Init_pulse_delay();
+	Initialize_Delay();
+	Initialize_Pre();
+	Initialize_Post();
 
 	if (TM_EXTI_Attach(GPIOA, GPIO_Pin_0, TM_EXTI_Trigger_Rising) == TM_EXTI_Result_Ok){
 		//TM_DISCO_LedOn(LED_RED);

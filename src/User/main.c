95cb5c82b021c961449a4b8be483b75c11c7982e
veresen <lolmariya@gmail.com>
2017-11-07 15:41:33 +0300

    Add communication with computer (not tested yet)

--- src/User/main.c
+++ src/User/main.c
@@ -20,23 +20,114 @@
  
 #include "main.h"
 
-uint8_t DELAY = 12; // in milliseconds
-uint8_t PULSE_DELAY = 40; // in milliseconds
-uint8_t TRAINING_TIME = 30; // in seconds (32 sec is max value)
+uint8_t auto_reload = 0;
 
-//PULSE_DELAY*1000-1 (PULSE_DELAY+DELAY)*1000-1
-//static TIM_Data training_timer = {41999, TRAINING_TIME*1000-1, TIM_CounterMode_Down, TIM5, RCC_APB1Periph_TIM5, TIM5_IRQn};
-static TIM_Data pulse_delay = {83, 0, TIM_CounterMode_Down, TIM5, RCC_APB1Periph_TIM5, TIM5_IRQn};
-static TIM_Data delay_timer = {83, 0, TIM_CounterMode_Up, TIM3, RCC_APB1Periph_TIM3, TIM3_IRQn};
-static TIM_Data pre_timer = {83, 19999, TIM_CounterMode_Up, TIM4, RCC_APB1Periph_TIM4, TIM4_IRQn};
-static TIM_Data post_timer = {83, 19999, TIM_CounterMode_Up, TIM2, RCC_APB1Periph_TIM2, TIM2_IRQn};
+uint8_t scale = 0;
+uint16_t bounds[] = {65, 655, 6553, 32768};
+uint16_t prescalers[] = {83, 839, 8339, 41999};
 
-void Init(){
-	pulse_delay.Period = (PULSE_DELAY+DELAY)*1000-1;
-	delay_timer.Period = DELAY*1000-1;
+uint8_t delay = 12; // in milliseconds
+uint8_t pulse_delay = 20; // in milliseconds
+uint8_t pulse = 20; // in milliseconds
+
+Impulse_Mode impulse_mode = MULTI;
+
+/**
+	Commands list.
+-- chmod: switch current mode between SINGLE and MULTI. No arguments.
+-- autorld: enables/disables auto reload of impulses after changes. Recieve 0 or 1 as argument.
+-- setdl: set delay between channels. Agument: unsigned integer.
+-- setpl: set pulse time. Argument: unsigned integer.
+-- setpdl: set delay between pulses. Argument: unsigned integer.
+*/
+char *commands[] = {"chmod", "autorld", "setdl", "setpl", "setpdl"};
+void (*handlers[])(uint8_t) = {&Set_Autoreload ,&Set_Delay, &Set_Pulse, &Set_Pulse_Delay};
+
+static TIM_Data pulse_delay_timer = {0, 0, TIM_CounterMode_Down, TIM5, RCC_APB1Periph_TIM5, TIM5_IRQn};
+static TIM_Data delay_timer = {0, 0, TIM_CounterMode_Up, TIM3, RCC_APB1Periph_TIM3, TIM3_IRQn};
+static TIM_Data pre_timer = {0, 0, TIM_CounterMode_Up, TIM4, RCC_APB1Periph_TIM4, TIM4_IRQn};
+static TIM_Data post_timer = {0, 0, TIM_CounterMode_Up, TIM2, RCC_APB1Periph_TIM2, TIM2_IRQn};
+static uint32_t turn_off = 0;
+static char buff[CMD_MAX_LEN];
+
+
+void Init_Button_Interrupt(){
+	// Clock for GPIOA
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+	// Clock for SYSCFG
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
+
+	// GPIOA initialization as an input from user button (GPIOA0)
+	GPIO_InitTypeDef GPIO_InitStruct;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_0;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_DOWN;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_2MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+	// Selects the GPIOA pin 0 used as external interrupt source
+	SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOA, EXTI_PinSource0);
+
+	// External interrupt settings
+	EXTI_InitTypeDef EXTI_InitStruct;
+	EXTI_InitStruct.EXTI_Line = EXTI_Line0;
+	EXTI_InitStruct.EXTI_LineCmd = ENABLE;
+	EXTI_InitStruct.EXTI_Mode = EXTI_Mode_Interrupt;
+	EXTI_InitStruct.EXTI_Trigger = EXTI_Trigger_Rising;
+	EXTI_Init(&EXTI_InitStruct);
+
+	// Nested vectored interrupt settings
+	NVIC_InitTypeDef NVIC_InitStruct;
+	NVIC_InitStruct.NVIC_IRQChannel = EXTI0_IRQn;
+	NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+	// EXTI0_IRQn has Most important interrupt
+	NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0x00;
+	NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0x00;
+	NVIC_Init(&NVIC_InitStruct);
 }
 
-void Init_Timer(TIM_Data *timer){
+void Init_LED(){
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+	GPIO_InitTypeDef gpioStructure;
+	gpioStructure.GPIO_Pin = GPIO_Pin_15 | GPIO_Pin_12 | GPIO_Pin_14;
+	gpioStructure.GPIO_Mode = GPIO_Mode_OUT;
+	gpioStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOD, &gpioStructure);
+}
+
+void Init_USART(){
+	GPIO_InitTypeDef gpio;
+	USART_InitTypeDef USART_InitStructure;
+	
+	__enable_irq();
+
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+
+	//GPIO_StructInit(&gpio);
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource2, GPIO_AF_USART2);
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource3, GPIO_AF_USART2);
+
+	gpio.GPIO_Mode = GPIO_Mode_AF;
+	gpio.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3;
+	gpio.GPIO_Speed = GPIO_Speed_50MHz;
+	gpio.GPIO_OType = GPIO_OType_PP;
+	gpio.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOA, &gpio);
+
+	USART_StructInit(&USART_InitStructure);
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+	USART_InitStructure.USART_BaudRate = BAUD_RATE;	
+	USART_Init(USART2, &USART_InitStructure);	
+
+	USART_ITConfig(USART2, USART_IT_RXNE, ENABLE);
+	
+	NVIC_EnableIRQ(USART2_IRQn);
+	USART_Cmd(USART2, ENABLE);
+}
+
+static void Init_Timer(TIM_Data *timer){
 	RCC_APB1PeriphClockCmd(timer->RCC_APB1Periph_TIM, ENABLE);
 	TIM_TimeBaseInitTypeDef timerInitStructure; 
 	timerInitStructure.TIM_Prescaler = timer->Prescaler;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          ;
@@ -54,47 +145,147 @@ void Init_Timer(TIM_Data *timer){
 	NVIC_Init(&nvicStructure);
 	
 	TIM_ClearITPendingBit (timer->TIM, TIM_IT_Update);
-  TIM_SetCounter (timer->TIM, 0);
-  TIM_ITConfig (timer->TIM, TIM_IT_Update, ENABLE);
+	TIM_SetCounter (timer->TIM, 0);
+	TIM_ITConfig (timer->TIM, TIM_IT_Update, ENABLE);
 }
 
-void Init_pulse_delay(){
-	Init_Timer(&pulse_delay);
+void Init_Pulse_Delay(){
+	Init_Timer(&pulse_delay_timer);
 }
 
-void Initialize_Delay(){
+void Init_Delay(){
 	Init_Timer(&delay_timer);
 }
 
-void Initialize_Post()
-{
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
-	GPIO_InitTypeDef gpioStructure;
-	gpioStructure.GPIO_Pin = GPIO_Pin_15;
-	gpioStructure.GPIO_Mode = GPIO_Mode_OUT;
-	gpioStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOD, &gpioStructure);
-	
+void Init_Post(){
 	Init_Timer(&post_timer);
 }
 
-void Initialize_Pre()
-{
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
-	GPIO_InitTypeDef gpioStructure;
-	gpioStructure.GPIO_Pin = GPIO_Pin_12;
-	gpioStructure.GPIO_Mode = GPIO_Mode_OUT;
-	gpioStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOD, &gpioStructure);
-
+void Init_Pre(){
 	Init_Timer(&pre_timer);
+}
+
+void Set_Periods(){
+	pulse_delay_timer.Period = (pulse_delay+delay)*1000-1;
+	pulse_delay_timer.Prescaler = prescalers[scale];
+	delay_timer.Period = delay*1000-1;
+	delay_timer.Prescaler = prescalers[scale];
+	pre_timer.Period = pulse*1000-1;
+	pre_timer.Prescaler = prescalers[scale];
+	post_timer.Period = pulse*1000-1;
+	post_timer.Prescaler = prescalers[scale];
+}
+
+void Init(){
+	Set_Periods();
+	Init_LED();
+	Init_Button_Interrupt();
+	Init_USART();
+	Init_Pulse_Delay();
+	Init_Delay();
+	Init_Pre();
+	Init_Post();
+}
+
+void ReinitDelays(){
+	Stop_Timers();
+	turn_off = 0;
+	Set_Periods();
+	if ((impulse_mode == MULTI) && auto_reload){
+		TIM_Cmd(TIM5, ENABLE);
+		turn_off = 1;
+	}
+}
+
+uint8_t Rescale(uint8_t time){
+	uint8_t is_valid = 0;
+	uint16_t *p = bounds;
+	do{
+		is_valid |= time < *p;
+		if (is_valid){
+			scale = p-bounds;
+			break;
+		}
+	}while(++p < bounds+4);
+	return is_valid;
+}
+
+void Set_Autoreload(uint8_t enable){
+	auto_reload = enable;
+}
+
+void Set_Delay(uint8_t value){
+	if (Rescale(value)){
+		delay = value;
+		ReinitDelays();
+	}
+}
+
+void Set_Pulse(uint8_t value){
+	if (Rescale(value)){
+		pulse = value;
+		ReinitDelays();
+	}
+}
+
+void Set_Pulse_Delay(uint8_t value){
+	if (Rescale(value)){
+		pulse_delay = value;
+		ReinitDelays();
+	}
+}
+
+uint8_t Parse_Apply_Cmd(char *cmd_str){
+	char *token = strtok(cmd_str, " ");
+	if (token != NULL){
+		for (int i = 1; i < 5; ++i){
+			if (strcmp(commands[i], token)){
+				handlers[i-1](strtol((token = strtok(NULL, " ")),NULL, 10));
+				return 1;
+			}
+		}
+	} else {
+		if (strcmp(commands[0],cmd_str)){
+			impulse_mode = !impulse_mode;
+			return 1;
+		}
+	}
+	return 0;
+}
+
+void USART2_IRQHandler(){
+	if (USART_GetITStatus(USART2, USART_IT_RXNE) == SET)
+	{
+		USART_ClearITPendingBit(USART2, USART_IT_RXNE);
+		static char *p_buff = buff;
+		if ((*p_buff = USART_ReceiveData(USART2)) == END_CHAR || p_buff < buff+CMD_MAX_LEN){
+			*p_buff = '\0';
+			if (Parse_Apply_Cmd(buff)){
+				Send_Message("OK");
+			} else {
+				Send_Message(strcat("Unresolved command", buff));
+			}
+			memset(buff, 0, CMD_MAX_LEN);
+			p_buff = buff;
+		} else {
+			p_buff++;
+		}
+	}
+}
+
+void Send_Message(char *msg){
+	int len = strlen(msg);
+	char *p_msg = msg;
+	while(p_msg < msg+len){
+		while(USART_GetITStatus(USART2, USART_IT_TXE) == SET);
+		USART_SendData(USART2, (*p_msg)++);
+	}
 }
 
 void TIM2_IRQHandler(){
 	if (TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET){
 		TIM_ClearITPendingBit(TIM2, TIM_IT_Update); 
 		GPIO_WriteBit(GPIOD, GPIO_Pin_15, Bit_RESET);
-		TM_DISCO_LedToggle(LED_ORANGE);
 		TIM_Cmd(TIM2, DISABLE);
 	}
 }
@@ -105,7 +296,6 @@ void TIM3_IRQHandler(){
 		TIM_Cmd(TIM3, DISABLE);
 		TIM_Cmd(TIM2, ENABLE);
 		GPIO_WriteBit(GPIOD, GPIO_Pin_15, Bit_SET);
-		TM_DISCO_LedOn(LED_ORANGE);
 	}
 }
 
@@ -113,7 +303,6 @@ void TIM4_IRQHandler(){
 	if (TIM_GetITStatus(TIM4, TIM_IT_Update) != RESET){
 		TIM_ClearITPendingBit(TIM4, TIM_IT_Update);
 		GPIO_WriteBit(GPIOD, GPIO_Pin_12, Bit_RESET);
-		TM_DISCO_LedToggle(LED_RED);
 		TIM_Cmd(TIM4, DISABLE);
 	}
 }
@@ -125,58 +314,76 @@ void TIM5_IRQHandler(){
 		}
 }
 
-inline void Stop_Timers(){
+void Start_Timers(){
+	TIM_Cmd(TIM4, ENABLE);
+	GPIO_WriteBit(GPIOD, GPIO_Pin_12, Bit_SET);
+	TIM_Cmd(TIM3, ENABLE);
+}
+
+void Stop_Timers(){
 	TIM_Cmd(TIM5, DISABLE);
 	TIM4_IRQHandler();
 	TIM_Cmd(TIM3, DISABLE);
 	TIM2_IRQHandler();
 }
 
-inline void Start_Timers(){
-	TIM_Cmd(TIM4, ENABLE);
-	GPIO_WriteBit(GPIOD, GPIO_Pin_12, Bit_SET);
-	TM_DISCO_LedOn(LED_RED);
-	TIM_Cmd(TIM3, ENABLE);
-}
-
-void TM_EXTI_Handler(uint16_t GPIO_Pin){
-	if (GPIO_Pin == GPIO_Pin_0){
-		if (Mode == MULTI){
-			static uint8_t stop_timer = 0;
-			if (stop_timer){
+void EXTI0_IRQHandler(){
+	if (EXTI_GetITStatus(EXTI_Line0) != RESET){
+		EXTI_ClearITPendingBit(EXTI_Line0);
+		if (impulse_mode == MULTI){
+			if (turn_off){
 				Stop_Timers();
 			} else {
 				TIM_Cmd(TIM5, ENABLE);
 			}
-			stop_timer = !stop_timer;
+			GPIO_ToggleBits(GPIOD, GPIO_Pin_14);
+			turn_off = !turn_off;
 		} else {
 			Start_Timers();
 		}
 	}
 }
 
-
-
 int main(void) {
 	//PD12 -- Pre, PD15 - Post
-
 	
 	/* Initialize system */
 	SystemInit();
-	TM_DISCO_LedInit();
-	TM_DISCO_ButtonInit();
 	
-	Init();
-	Init_pulse_delay();
-	Initialize_Delay();
-	Initialize_Pre();
-	Initialize_Post();
-
-	if (TM_EXTI_Attach(GPIOA, GPIO_Pin_0, TM_EXTI_Trigger_Rising) == TM_EXTI_Result_Ok){
-	}
+	//Init();
+	Set_Periods();
+	Init_LED();
+	Init_Button_Interrupt();
+	//Init_USART();
+	Init_Pulse_Delay();
+	Init_Delay();
+	Init_Pre();
+	Init_Post();
 	
 	while (1){
 	
 	}
 }
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
 
--- src/User/main.h
+++ src/User/main.h
@@ -2,16 +2,25 @@
 #define MAIN_H
 
 #include "stm32f4xx.h"
-#include "defines.h"
-#include "tm_stm32f4_disco.h"
-#include "tm_stm32f4_exti.h"
+#include "misc.h"
+#include "stm32f4xx.h"
+#include "stm32f4xx_rcc.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_usart.h"
+#include "stm32f4xx_tim.h"
+#include "stm32f4xx_syscfg.h"
+#include "stm32f4xx_exti.h"
+#include <string.h>
+#include <stdlib.h>
+
+#define BAUD_RATE 9600
+#define CMD_MAX_LEN 14
+#define END_CHAR 'x'
 
 typedef enum {
-	SINGLE,
-	MULTI
+	SINGLE = 0,
+	MULTI = !SINGLE
 } Impulse_Mode;
-
-Impulse_Mode Mode = MULTI;
 
 typedef struct {
 	uint16_t Prescaler;
@@ -22,6 +31,23 @@ typedef struct {
 	uint8_t NVIC_TIM_IRQChannel;
 } TIM_Data;
 
-inline void Start_Timers(void);
-inline void Stop_Timers(void);
+void Start_Timers(void);
+void Stop_Timers(void);
+uint8_t Rescale(uint8_t);
+void Set_Autoreload(uint8_t);
+void Set_Delay(uint8_t);
+void Set_Pulse(uint8_t);
+void Set_Pulse_Delay(uint8_t); // this is not an union of two functions above
+uint8_t Parse_Apply_Cmd(char*);
+void Send_Message(char *);
+void ReinitDelays(void);
+void Init(void);
+void Init_Pre(void);
+void Init_Post(void);
+void Init_Delay(void);
+void Init_Pulse_Delay(void);
+void Init_USART(void);
+void Init_LED(void);
+void Init_Button_Interrupt(void);
+
 #endif
